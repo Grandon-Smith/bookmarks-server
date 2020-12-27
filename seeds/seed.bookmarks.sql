3a4aafbbbf0129d653082d5fe679118617c119ed
Grandon Smith <grandon.smith@yahoo.com>
2020-12-27 11:44:21 -0500

    so many things involved in making a server...

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,13 @@
+INSERT INTO bookmarks-test (id, title, url, rating, description)
+VALUES
+    ('0', 'website title', 
+    'http://www.google.com', '3',
+    'Internet-related services and products.')
+    
+    ('1', 'another website title', 
+    'http://www.google.com', '4',
+    'Internet-related services and products.')
+
+    ('2', 'third website title', 
+    'http://www.google.com', '2',
+    'Internet-related services and products.')
